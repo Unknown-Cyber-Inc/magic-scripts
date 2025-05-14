b034ae54be44a81d3d3324a6938b6071434a046b
Arun Lakhotia <arun@louisiana.ed>
2025-05-14 16:35:02 -0500

    Add scripts to fetch yara

---
+++ get-multifile-yara.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+# TO GET INFORMATION ABOUT A FILE
+
+# USAGE
+#   get-file-genomics.sh $sha1-file-hash
+
+
+sha1_list=$1
+
+
+# CUSTOMIZABLE PARAMETERS
+# COMMENT/UNCOMMENT FOLLOWING DEPENDING ON USE
+
+f=""
+f="read_mask=_default"  # see documentation
+
+read_mask=$f
+
+x=""
+#x="expand_mask=notes"  # traverse notes object and get the notes
+
+
+
+no_links=true               # true: do not give links to related objects
+
+expand_mask=$x
+
+curl -H "Authorization: Bearer $MAGIC_ACCESS_TOKEN" \
+    -F "files=$sha1_list" \
+    -X POST \
+    "$MAGIC_API/v2/files/yara/?$read_mask&expand_mask=$x&no_links=$no_links"
